d1fb6d3f793deb07154b14d549dfb20812a4dd5f
CardenDang <dangcarden@gmail.com>
2026-08-28 16:00:55 -0700

    Setup

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+function Home() {
+    return (
+        <main className="home">
+            <div>
+
+                
+            </div>
+            <h1 className="main-title">
+                CD
+            </h1>
+
+            <Link to="/about" className="about-btn">
+                about
+            </Link>
+
+            <Link to="/experience" className="experience-btn">
+                experience
+            </Link>
+
+            <Link to="/projects" className="projects-btn">
+                projects
+            </Link>
+
+            <Link to="/skills" className="skills-btn">
+                skills
+            </Link>
+
+            <Link to="/contacts" className="contacts-btn">
+                contacts
+            </Link>
+
+
+        </main>
+    );
+}
+
+export default Home;
